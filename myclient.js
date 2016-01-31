bd12b55252a68969416be1c90033703bfcfc25e0
edward <edward@theemm.com>
2016-01-30 22:03:03 -0800

    pulled down code from the sample site.

--- myclient.js
+++ myclient.js
@@ -1,11 +1,34 @@
-window.onload = function () {
-  var url, i;
+/**
+ * MYCLIENT.JS
+ * an example web app using an ajax request to our API server which returns a JSON object 
+ * 
+ * When a user opens index.html it then loads and executes this JavaScript code, which reads 
+ * the current logic level on the input ports, and displays that in the window, each in a div 
+ * identified by the index "inputs_[port number]"
+ */
 
-  for (i = 0; i < 2; i++) {
-    url = document.URL + 'inputs/' + i;
-    $.getJSON(url, function (data) {
-      console.log('API response received');
-      $('#input').append('<p>input gpio port ' + data.gpio + ' on pin ' + data.pin + ' has current value ' + data.value + '</p>');
-    });
+window.onload = function () {
+  var url,
+    i,
+    ports = [23, 25];  // the GPIO ports we will read
+
+  for (i in ports) {
+    $('#input_' + ports[i]).html('loading port ' + ports[i] + ' value...');
   }
-};
+
+  setInterval( function () {
+    for (i in ports) {
+      url = document.URL + 'inputs/' + ports[i];
+      console.log('making API call ' + url);
+
+      $.getJSON(url, function (data) {
+        console.log('API response received. port ' + data.gpio + ' value = ' + data.value);
+        $('#input_' + data.gpio).html('GPIO input port ' + data.gpio + ' value is ' + data.value);
+      });
+    } // for 
+  }, 1000); // setInterval
+  
+}; //onload
+
+
+
