168a540d88306ecd0e684a9caa74a261e29f9016
edward <edward@theemm.com>
2016-01-31 12:33:08 -0800

    testing.

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 var gpio = require('rpi-gpio');
  
-gpio.setup(4, gpio.DIR_OUT, write);
+gpio.setup(4, gpio.DIR_IN, readInput);
  
+function readInput() {
+    gpio.read(4, function(err, value) {
+        console.log('The value is ' + value);
+    });
+}
+
+gpio.setup(4, gpio.DIR_OUT, write);
 function write() {
     gpio.write(4, true, function(err) {
         if (err) throw err;
