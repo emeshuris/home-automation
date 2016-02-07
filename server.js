c81a5f9d88af92365d800a2891220acb56abfe54
Edward M. Meshuris <edward@theemm.com>
2016-02-07 06:19:02 -0800

    .

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function pushToAry(name, val) {
 function getPin(name) {
     for (var key in pins) {
         if (key == name) {
-            console.log('im here');
+            console.log('im here: ' + key);
             return pins[key];
         }
     }
