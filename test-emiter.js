0d027c73bf2baa81a6ce87677a05f59ea9896326
Edward M. Meshuris <edward@theemm.com>
2016-02-07 03:21:11 -0800

    .

--- test-emiter.js
+++ test-emiter.js
@@ -3,4 +3,5 @@ var gpio = require('rpi-gpio');
 gpio.on('change', function(channel, value) {
 	console.log('Channel ' + channel + ' value is now ' + value);
 });
+
 gpio.setup(8, gpio.DIR_IN, gpio.EDGE_BOTH);
