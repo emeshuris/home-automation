b7f5ccd0e18f3cb251026223b4a85c6fbe113ada
Edward M. Meshuris <edward@theemm.com>
2016-02-14 09:26:40 -0800

    .

--- test-emiter.js
+++ test-emiter.js
@@ -1,38 +1,23 @@
 var gpio = require('rpi-gpio');
- 
-var pin   = 10;
- 
-gpio.setup(pin, gpio.DIR_OUT);
 
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
-gpio.write(pin, 1);
-gpio.write(pin, 0);
+var pin = 10;
+var delay = 2000;
+var count = 0;
+var max = 3;
+
+gpio.setup(pin, gpio.DIR_OUT, on);
+gpio.setup(pin, gpio.DIR_OUT, off);
+gpio.setup(pin, gpio.DIR_OUT, on);
+gpio.setup(pin, gpio.DIR_OUT, off);
+
+function on() {
+    setTimeout(function () {
+        gpio.write(pin, 1);
+    }, delay);
+}
+
+function off() {
+    setTimeout(function () {
+        gpio.write(pin, 0);
+    }, delay);
+}
