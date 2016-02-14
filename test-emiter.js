5e548d7b47a70e9d07416e822eee10675144ec92
Edward M. Meshuris <edward@theemm.com>
2016-02-14 09:08:54 -0800

    .

--- test-emiter.js
+++ test-emiter.js
@@ -3,6 +3,10 @@ gpio.destroy();
 var pin = 10;
 
 gpio.setup(pin, gpio.DIR_OUT, write);
+gpio.setup(pin, gpio.DIR_OUT, write);
+gpio.setup(pin, gpio.DIR_OUT, write);
+gpio.setup(pin, gpio.DIR_OUT, write);
+gpio.setup(pin, gpio.DIR_OUT, write);
  
 function write() {
     gpio.write(pin, true, function(err) {
