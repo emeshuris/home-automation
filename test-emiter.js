939ac060ef4ceba20177796f13cbaea892d916b7
Edward M. Meshuris <edward@theemm.com>
2016-02-14 09:10:15 -0800

    .

--- test-emiter.js
+++ test-emiter.js
@@ -5,7 +5,7 @@ var pin = 10;
 gpio.setup(pin, gpio.DIR_OUT);
  
 function write() {
-    gpio.write(pin, true, function(err) {
+    gpio.write(pin, false, function(err) {
         if (err) throw err;
         console.log('Written to pin: ' + pin);
     });
