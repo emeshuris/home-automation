c326effe476c1a60691578d11fff8965da1fb3b8
Edward M. Meshuris <edward@theemm.com>
2016-02-14 09:22:01 -0800

    .

--- test-emiter.js
+++ test-emiter.js
@@ -1,28 +1,38 @@
 var gpio = require('rpi-gpio');
  
 var pin   = 10;
-var delay = 2000;
-var count = 0;
-var max   = 3;
  
-gpio.setup(pin, gpio.DIR_OUT, on);
- 
-function on() {
-    if (count >= max) {
-        gpio.destroy(function() {
-            console.log('Closed pins, now exit');
-        });
-        return;
-    }
- 
-    setTimeout(function() {
-        gpio.write(pin, 1, off);
-        count += 1;
-    }, delay);
-}
- 
-function off() {
-    setTimeout(function() {
-        gpio.write(pin, 0, on);
-    }, delay);
-}
+gpio.setup(pin, gpio.DIR_OUT);
+
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
+gpio.write(pin, 1);
+gpio.write(pin, 0);
