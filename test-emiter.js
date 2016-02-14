46a1ce69685fd5e793b28c74b733859cbb9c9494
Edward M. Meshuris <edward@theemm.com>
2016-02-14 08:36:32 -0800

    .

--- test-emiter.js
+++ test-emiter.js
@@ -1,6 +1,22 @@
-var fastgpio = require( "fastgpio" );
-var pin = 15;
+var gpio = require("pi-gpio");
+var pin = 10;
 
-fastgpio.prepareGPIO(pin);
-fastgpio.set(pin);
-fastgpio.unset(pin);
+function flashLED(pin, duration) {
+    return setInterval(function() {
+        gpio.open(pin, "output", function(err) {
+            gpio.write(pin, pin, function() {
+                setTimeout(function() {
+                    gpio.write(pin, 0, function(err) {
+                        gpio.close(pin);
+                    });
+                }, duration/2);
+            });
+        });
+    }, duration);
+}
+
+var intervalID = flashLED(7, 500);
+
+setTimeout(function() {
+    clearInterval(intervalID);
+}, 60000);
